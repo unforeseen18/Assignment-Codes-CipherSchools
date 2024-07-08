57a6abfbca15ffb4cd3d81b7e94313effd3a82c0
Prajakta Chhetri <115145849+unforeseen18@users.noreply.github.com>
2024-07-08 18:31:04 +0530

    Lecture 3.java

---
+++ Lecture 3.java
@@ -0,0 +1,25 @@
+package CoreJava;
+
+public class PrimitiveDT {
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 7;
+        
+
+        System.out.println("X is: " + x);
+        System.out.println("Y is: " + y);
+        
+        byte b = 10;
+        System.out.println("B is: " + b);
+        
+        byte b2 = (byte) 129;
+        System.out.println("B2 is: " + b2);
+        
+        byte b3 = (byte) 128;
+        System.out.println("B3 is: " + b3);
+        
+        int i = b;
+        System.out.println("I is: " + i);
+    }
+}
